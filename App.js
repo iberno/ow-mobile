0a0a1c125d8bff89edb20538cddb9fce35c00d1a
Iberno Hoffmann <iberno@outlook.com>
2020-01-20 17:33:36 -0300

    app finished

--- App.js
+++ App.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, YellowBox } from 'react-native';
+
+YellowBox.ignoreWarnings([
+  'Unrecognized WebSocket',
+  //'initialRegion.latitudeDelta'
+]);
+
+import Routes from './src/routes';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <>
+    <StatusBar barStyle="light-content" backgroundColor="#7d40e7" />
+    <Routes />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
